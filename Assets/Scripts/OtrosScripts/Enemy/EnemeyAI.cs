05c44f9f312eb695a3b7d9023433e5e17db0624f
Didac <didacfernandeztvjb12013@cev.com>
2015-05-10 22:41:01 +0200

    Reorganized Collisions

--- Assets/Scripts/OtrosScripts/Enemy/EnemeyAI.cs
+++ Assets/Scripts/OtrosScripts/Enemy/EnemeyAI.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class EnemeyAI : MonoBehaviour 
 {
     private float speed = 0.5f;
+	public float range = 3.0f;
     public GameObject player;
 	
 	void Update () 
     {
         float viewDistance = Vector3.Distance(player.transform.position, transform.position);
 
-        if (viewDistance <= 3)
+        if (viewDistance <= range)
         {
             if (player.transform.position.x >= transform.position.x)
                 transform.position += new Vector3(speed,0,0) * Time.deltaTime;
