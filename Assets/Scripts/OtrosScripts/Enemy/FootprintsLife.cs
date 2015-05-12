04b08bba9450ff94d9c96e21d1c30943c25425da
Didac <didacfernandeztvjb12013@cev.com>
2015-05-12 13:12:05 +0200

    Zombie IA actualized
    Pending alpha footrprints

---
+++ Assets/Scripts/OtrosScripts/Enemy/FootprintsLife.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootprintsLife : MonoBehaviour 
+{
+	private float lifeCounter = 0;
+	
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		lifeCounter++;
+
+		if (lifeCounter >= 400)
+		{
+			DestroyObject(this.gameObject);
+		}
+	}
+}
