8613642357bc4c091b99eb69249bea9be69896c2
Didac <didacfernandeztvjb12013@cev.com>
2015-05-13 19:42:34 +0200

    Bear Traps 2.0

--- Assets/Scripts/OtrosScripts/Enemy/FootprintsLife.cs
+++ Assets/Scripts/OtrosScripts/Enemy/FootprintsLife.cs
@@ -4,19 +4,18 @@ using System.Collections;
 public class FootprintsLife : MonoBehaviour 
 {
 	private float lifeCounter = 0;
-	
-	void Start () 
-	{
-
-	}
 
 	void Update () 
 	{
 		lifeCounter++;
 
+		if (lifeCounter >= 200)
+			GetComponent<Renderer> ().material.color = new Color(1, 1, 1, 0.5f);
+
 		if (lifeCounter >= 400)
-		{
+			GetComponent<Renderer> ().material.color = new Color(1, 1, 1, 0.25f);
+
+		if (lifeCounter >= 500)
 			DestroyObject(this.gameObject);
-		}
 	}
 }
