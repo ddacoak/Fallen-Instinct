c85352d0f1f2d83cefc605eb6b7b49c0ba68446a
Didac <didacfernandeztvjb12013@cev.com>
2015-05-14 00:02:52 +0200

    Bear traps like enemy weapons

--- Assets/Scripts/OtrosScripts/Interactuables/Cepos.cs
+++ Assets/Scripts/OtrosScripts/Interactuables/Cepos.cs
@@ -6,8 +6,10 @@ public class Cepos : MonoBehaviour
 	Animator anim;
 	private int valorCambio = 0;
 
-	public GameObject player;
+	public GameObject playerShadow;
 	public float range = 0.5f;
+
+	public GameObject player;
 
 	//Damage
 	public int power = 100;
@@ -19,12 +21,28 @@ public class Cepos : MonoBehaviour
 
 	void Update () 
 	{
-		float viewDistance = Vector3.Distance(player.transform.position, transform.position);
+		float viewDistance = Vector3.Distance(playerShadow.transform.position, transform.position);
 
 		if (viewDistance <= range) 
 		{
 			valorCambio = 1;
 			anim.SetInteger("Detect", valorCambio);
 		}
+
+		//DetectCollision (other:gameObject);
 	}
+
+	/*void DetectCollision(GameObject other) 
+	{
+		Life otherLife = other.GetComponent("Life") as Life;
+
+		if (valorCambio == 1)
+		{
+			Life myLife = gameObject.GetComponent("Life") as Life;
+			//If I am alive, and the other object is not a weapon attack and the other object 
+			//aligment is different to mine does damage
+			if (myLife.life>0 && otherLife.type!=TYPE.WEAPON &&	otherLife.aligment!=myLife.aligment)
+				otherLife.Hit(power);
+		}
+	}*/
 }
